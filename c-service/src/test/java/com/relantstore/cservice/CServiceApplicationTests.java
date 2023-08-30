419e21cc4d45b640d0d51093a497c50b2dde555a
oaps93_surfacehome <oaps93@gmail.com>
2023-08-30 17:18:33 -0600

    new customer service structure added

---
+++ c-service/src/test/java/com/relantstore/cservice/CServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.relantstore.cservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
